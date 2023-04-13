e20ae50e2592b5d2ec9a021765a441e06f2b834b
Maxwell Alvord <maxwellalvord@gmail.com>
2023-04-13 11:05:42 -0700

    Update: update App file with routing.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,30 @@
 
+import { Box } from "@mui/material";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "./theme";
-
+import Navbar from "@/scenes/navbar";
+// import Dashboard from "@/scenes/dashboard";
+// import Predictions from "@/scenes/predictions";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
+      <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div/>} />
+              <Route path="/predictions" element={<div />} />
+            </Routes>
+          </Box>
         </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
