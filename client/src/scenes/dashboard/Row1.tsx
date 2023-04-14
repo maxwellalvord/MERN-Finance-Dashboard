971a625683df6e39af88528e96ec7153667f71e6
Maxwell Alvord <maxwellalvord@gmail.com>
2023-04-14 14:40:24 -0700

    Update: build out chart for grid area "c"

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -3,6 +3,8 @@ import { useGetKpisQuery } from "@/state/api"
 import {
   ResponsiveContainer,
   AreaChart,
+  BarChart,
+  Bar,
   XAxis,
   CartesianGrid,
   YAxis,
@@ -18,10 +20,23 @@ import BoxHeader from "@/components/BoxHeader";
 
 type Props = {}
 
-const Row1 = (props: Props) => {
+const Row1 = () => {
 
   const { palette } = useTheme();
   const { data } = useGetKpisQuery();
+
+  const revenue = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: revenue,
+        }
+      })
+    )
+  }, [data]);
+
   const revenueExpenses = useMemo(() => {
     return (
       data &&
@@ -184,9 +199,57 @@ const Row1 = (props: Props) => {
           </LineChart>
         </ResponsiveContainer>
       </DashboardBox>
-      <DashboardBox gridArea="c"></DashboardBox>
+      <DashboardBox gridArea="c">
+        <BoxHeader
+          title="Revenue Month by Month"
+          subtitle="graph representing the revenue month by month"
+          sideText="+4%"
+        />
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={300}
+            data={revenue}
+            margin={{
+              top: 17,
+              right: 15,
+              left: -5,
+              bottom: 58,
+            }}
+          >
+            <defs>
+              <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0}
+                />
+              </linearGradient>
+            </defs>
+            <CartesianGrid vertical={false} stroke={palette.grey[800]} />
+            <XAxis
+              dataKey="name"
+              axisLine={false}
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              axisLine={false}
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <Tooltip />
+            <Bar dataKey="revenue" fill="url(#colorRevenue)" />
+          </BarChart>
+        </ResponsiveContainer>
+      </DashboardBox>
     </>
-  )
-}
+  );
+};
 
-export default Row1
+export default Row1;
