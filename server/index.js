6c8efefb0c0bc139576cfa675e316be56132a02a
Maxwell Alvord <maxwellalvord@gmail.com>
2023-04-14 15:41:24 -0700

    Update: build out the transactions API route/ Typing.

--- server/index.js
+++ server/index.js
@@ -6,10 +6,12 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js"
+import transactionRoutes from "./routes/transaction.js"
 import KPI from "./models/KPI.js";
 import Product from "./models/Product.js"
+import Transaction from "./models/Transaction.js";
 import productRoutes from "./routes/product.js";
-import { kpis, products } from "./data/data.js";
+import { kpis, products, transactions } from "./data/data.js";
 
 // configs
 dotenv.config();
@@ -25,6 +27,7 @@ app.use(cors());
 // Routes
 app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
 // Mongoose config
 const PORT = process.env.PORT || 9000;
@@ -40,5 +43,6 @@ mongoose
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
     // Product.insertMany(products);
+    // Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connect`));
